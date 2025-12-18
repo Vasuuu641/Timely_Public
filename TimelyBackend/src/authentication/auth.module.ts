eeaff9cf03e645e2a58bcbdd066ae4ec2c50b388
Vasundhara Ravi <vasundhararavi641@gmail.com>
2025-12-18 21:36:13 +0100

    Adding JWT tokens to store user login session state

---
+++ TimelyBackend/src/authentication/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';      
+import { PassportModule } from '@nestjs/passport';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+    imports: [
+        UserModule,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+        secret: 'process.env.JWT_SECRET',
+        signOptions: { expiresIn: '60m' },
+        }),
+        ],  
+    controllers: [AuthController],
+    providers: [AuthService, JwtStrategy],
+})
+export class AuthModule {}
